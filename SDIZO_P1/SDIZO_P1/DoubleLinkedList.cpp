f5b0823e785203ec918d1a52e994f71c4208588a
krzysiekmwk <krzysiek.mwk@gmail.com>
2018-03-22 21:21:37 +0100

    added comments in double linked list

--- SDIZO_P1/SDIZO_P1/DoubleLinkedList.cpp
+++ SDIZO_P1/SDIZO_P1/DoubleLinkedList.cpp
@@ -1,6 +1,6 @@
 #include "DoubleLinkedList.h"
 
-ElemList::ElemList() {
+ElemList::ElemList() {	//constructor of helper class.
 	data = 0;
 	next = nullptr;
 	prev = nullptr;
@@ -14,33 +14,33 @@ DoubleLinkedList::DoubleLinkedList() {
 
 DoubleLinkedList::~DoubleLinkedList()
 {
-	clear();
+	clear();	//In destructor we need to clear all data
 }
 
 int DoubleLinkedList::front() {
 	if (amount == 0)
 		return -1;
-	return head->data;
+	return head->data;	//return last element
 }
 
 int DoubleLinkedList::back() {
 	if (amount == 0)
 		return -1;
-	return tail->data;
+	return tail->data;	// return first element
 }
 
 void DoubleLinkedList::pushFront(int data) {
-	ElemList * elem = new ElemList;
+	ElemList * elem = new ElemList;	//Create new element
 	elem->data = data;
 	elem->prev = nullptr;
-	elem->next = head;
+	elem->next = head;	// element hav pointer for previous element
 
-	if (head != nullptr)
-		head->prev = elem;
+	if (head != nullptr)	//if list is not empty assign new element to pointer in previous object
+		head->prev = elem;	// Element will be as first element in the list
 	else
-		tail = elem;
+		tail = elem;	// if list was empty, the tail is pointer for actual element
 
-	head = elem;
+	head = elem;	// new element is new head
 
 	amount++;
 }
@@ -48,15 +48,15 @@ void DoubleLinkedList::pushFront(int data) {
 void DoubleLinkedList::pushBack(int data) {
 	ElemList * elem = new ElemList;
 	elem->data = data;
-	elem->prev = tail;
+	elem->prev = tail;	// have pointer to tail in list.
 	elem->next = nullptr;
 
 	if (tail != nullptr)
-		tail->next = elem;
+		tail->next = elem;	// add as last element at 0 position
 	else
 		head = elem;
 
-	tail = elem;
+	tail = elem;	// new element is new tail
 
 	amount++;
 }
@@ -65,22 +65,23 @@ void DoubleLinkedList::pushAtPosition(int data, int position) {
 	if (tail == nullptr)
 		pushFront(data);
 	else {
-		if (position <= 0) {
+		if (position <= 0) {	//if index is wrong and smaller than 0 push it as first element
 			pushBack(data);
 			return;
 		}
-		if (position >= amount) {
+		if (position >= amount) { //if index is wrong and bigger than sizse of list push it as last element
 			pushFront(data);
 			return;
 		}
-			
+		
+		// start searching from 0 position. from tail.
 		int i = 0;
-		ElemList *tmp = tail; //Don't change the tail. 
+		ElemList *tmp = tail; //Don't change the tail. There is only pointer which is poining for tail
 		while (tmp != nullptr) {
-			if (i == position) {
+			if (i == position) {	//if there such position then insert new element there
 				ElemList * elem = new ElemList;
 				elem->data = data;
-				elem->next = tmp->next;
+				elem->next = tmp->next; //push beetween two of elements
 				elem->prev = tmp;
 				tmp->next->prev = elem;
 				tmp->next = elem;
@@ -89,7 +90,7 @@ void DoubleLinkedList::pushAtPosition(int data, int position) {
 				break;
 			}
 			i++;
-			tmp = tmp->prev;
+			tmp = tmp->prev;	// move pointer to the next element in list
 		}
 	}
 }
@@ -98,9 +99,9 @@ void DoubleLinkedList::popFront() {
 	if (amount == 0)
 		return;
 	if (head->next != nullptr) {
-		ElemList * newHead = head->next;
+		ElemList * newHead = head->next;	// pointer for new haed which is just previous value
 		newHead->prev = nullptr;
-		delete head;
+		delete head;	// release memory for old object
 		head = newHead;
 	}
 	else{
@@ -140,7 +141,7 @@ void DoubleLinkedList::popAtPosition(int position) {
 		}
 
 		int i = 0;
-		ElemList *tmp = tail; //Don't change the tail. 
+		ElemList *tmp = tail; //Very simmilar to pushing at specifiet position. except there we will delete old data
 		while (tmp != nullptr) {
 			if (i == position) {
 				ElemList * elem = tmp->prev;
@@ -170,7 +171,7 @@ int DoubleLinkedList::size() {
 
 int DoubleLinkedList::at(int index) {
 	int i = 0;
-	ElemList *tmp = tail; //Enumerate from beginig of List
+	ElemList *tmp = tail; //Enumerate from beginig of List. Just like an array. from 0 to size
 	while (tmp != nullptr){
 		if (i == index)
 			return tmp->data;
@@ -193,7 +194,7 @@ bool DoubleLinkedList::contains(int data) {
 	return false;
 }
 
-void DoubleLinkedList::clear() {
+void DoubleLinkedList::clear() {	// Pop all elements just to be sure to release all memory
 	while (amount > 0) {
 		popFront();
 	}
